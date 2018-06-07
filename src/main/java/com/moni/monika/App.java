7b373e1a2d0a1030d127302e60de1ef401a260d2
monikarana1974 <moneekarani@gmail.com>
2018-06-07 09:34:10 +0000

    appjava

---
+++ src/main/java/com/moni/monika/App.java
@@ -0,0 +1,13 @@
+package com.moni.monika;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
